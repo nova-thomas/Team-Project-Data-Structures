d7e350133adb7e76d8561662b8e6077e784837f7
LikaVoyt <114115664+LikaVoyt@users.noreply.github.com>
2023-11-17 12:54:16 -0500

    Update Team Project Data Structures.cpp

--- Team Project Data Structures/Team Project Data Structures.cpp
+++ Team Project Data Structures/Team Project Data Structures.cpp
@@ -85,3 +85,74 @@ int main()
 
     return 0;
 }
+
+
+
+
+
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <fstream>
+
+using namespace std;
+
+// Function called in the creation of a thread object to handle the job given by the input in main
+vector<pair<int, int>> parallelizeJobs(int n, int m, const vector<int>& processingTime) {
+    vector<int> threadTime(n, 0); // initialize starting times for each thread
+    vector<pair<int, int>> jobSchedule;
+
+    for (int i = 0; i < m; ++i) {
+        int currentJobTime = processingTime[i];
+        auto minThreadIndex = min_element(threadTime.begin(), threadTime.end()) - threadTime.begin();
+        jobSchedule.push_back(make_pair(minThreadIndex, threadTime[minThreadIndex])); // record job schedule
+        threadTime[minThreadIndex] += currentJobTime;
+    }
+    return jobSchedule;
+}
+
+int main() {
+    // Variables
+    int n, m; // n number of threads, m number of jobs
+    vector<int> times; // Using m as the index to match the job to the vector list of times
+    string inputFileName;
+
+    // Prompt for input file name
+    cout << "Enter the name of the file: ";
+    cin >> inputFileName;
+
+    // File input
+    ifstream inputFile(inputFileName);
+
+    // Checking if opened properly
+    if (!inputFile.is_open()) {
+        cerr << "Error opening the file." << endl;
+        return 1;
+    }
+
+    // Read n and m from file
+    inputFile >> n >> m;
+
+    // Resizing the vector list according to input file
+    times.resize(m);
+
+    // Reading m time numbers from file and stores in vector
+    for (int i = 0; i < m; i++) {
+        inputFile >> times[i];
+    }
+
+    // Creating threads and feeding the input to the threads
+    auto outputSchedule = parallelizeJobs(n, m, times);
+
+    // Output of threads
+    for (const auto& job : outputSchedule) {
+        cout << job.first << " " << job.second << endl;
+    }
+
+    // Closing file
+    inputFile.close();
+
+    return 0;
+}
+
