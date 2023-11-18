1e085092a27841ff2e4ffde8aeb483eb5bcb4f3a
Nova Thomas <97986758+nova-thomas@users.noreply.github.com>
2023-11-18 16:07:45 -0500

    added initialization output of threads "0 0"

--- Team Project Data Structures/Team Project Data Structures.cpp
+++ Team Project Data Structures/Team Project Data Structures.cpp
@@ -35,6 +35,7 @@ vector<pair<int, long>> parallelizeJobs(int n, int m, const vector<long>& proces
 
     for (int i = 0; i < n; ++i) {
         threadTime.push({ 0, i }); // initialize starting times for each thread
+        cout << 0 << " " << 0 << endl;
     }
 
     for (int i = 0; i < m; ++i) {
@@ -42,8 +43,8 @@ vector<pair<int, long>> parallelizeJobs(int n, int m, const vector<long>& proces
         auto minThread = threadTime.top();
         threadTime.pop();
 
-        jobSchedule.push_back({ minThread.second, max(minThread.first, 0L) });
-        minThread.first += currentJobTime;
+        jobSchedule.push_back({ minThread.second, max(minThread.first, 0L) + currentJobTime }); // Updated this line
+        minThread.first = max(minThread.first, 0L) + currentJobTime; // Updated this line
         threadTime.push(minThread);
     }
     return jobSchedule;
@@ -96,84 +97,3 @@ int main()
 
     return 0;
 }
-
-
-
-#include <iostream>
-#include <vector>
-#include <algorithm>
-#include <fstream>
-#include <queue>
-
-using namespace std;
-
-// Function called in the creation of a thread object to handle the job given by the input in main
-vector<pair<int, long>> parallelizeJobs(int n, int m, const vector<long>& processingTime) {
-    // Priority queue to store thread times
-    priority_queue<pair<long, int>, vector<pair<long, int>>, greater<pair<long, int>>> threadTime;
-
-    vector<pair<int, long>> jobSchedule;
-
-    for (int i = 0; i < n; ++i) {
-        threadTime.push({ 0, i }); // initialize starting times for each thread
-    }
-
-    for (int i = 0; i < m; ++i) {
-        long currentJobTime = processingTime[i];
-        auto minThread = threadTime.top();
-        threadTime.pop();
-
-        jobSchedule.push_back({ minThread.second, max(minThread.first, 0L) + currentJobTime }); // Updated this line
-        minThread.first = max(minThread.first, 0L) + currentJobTime; // Updated this line
-        threadTime.push(minThread);
-    }
-    return jobSchedule;
-}
-
-int main() {
-    // Variables
-    int n, m; // n number of threads, m number of jobs
-    vector<long> times; // Using m as the index to match the job to the vector list of times
-    string inputFileName;
-
-    // Prompt for input file name
-    cout << "Enter the name of the file: ";
-    cin >> inputFileName;
-
-    // File input
-    ifstream inputFile(inputFileName);
-
-    // Checking if opened properly
-    if (!inputFile.is_open()) {
-        cerr << "Error opening the file." << endl;
-        return 1;
-    }
-
-    // Read n and m from file
-    inputFile >> n >> m;
-
-    // Check if the reading was successful
-    if (inputFile.fail()) {
-        cerr << "Error reading values from the file." << endl;
-        return 1;
-    }
-
-    // Resizing the vector list according to input file
-    times.resize(m);
-
-    // Reading m time numbers from file and stores in vector
-    for (int i = 0; i < m; i++) {
-        inputFile >> times[i];
-    }
-
-    // Creating threads and feeding the input to the threads
-    auto outputSchedule = parallelizeJobs(n, m, times);
-
-    // Output of threads
-    for (const auto& job : outputSchedule) {
-        cout << job.first << " " << job.second << endl;
-    }
-
-    return 0;
-}
-
