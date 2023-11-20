8a381bb5ec16319817689e96894cf14d1f16b8cd
KelseaWasHere <150960092+KelseaWasHere@users.noreply.github.com>
2023-11-20 12:12:11 -0500

    fixed the issue

--- Team Project Data Structures/Team Project Data Structures.cpp
+++ Team Project Data Structures/Team Project Data Structures.cpp
@@ -43,11 +43,11 @@ vector<pair<int, long>> parallelizeJobs(int n, int m, const vector<long>& proces
         auto minThread = threadTime.top();
         threadTime.pop();
 
-        // -------------------- Somewhere around here the logic is causing an overflow error I believe, because it keeps printing some random negative numbers
-        jobSchedule.push_back({ minThread.second, max(minThread.first, 0L) + currentJobTime });
-        minThread.first = max(minThread.first, 0L) + currentJobTime;
+        jobSchedule.push_back({ minThread.second, minThread.first });
+        minThread.first += currentJobTime;
         threadTime.push(minThread);
     }
+
     return jobSchedule;
 }
 
